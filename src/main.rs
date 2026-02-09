8fa6f15faea8c9fa9b39cbb4ee47c2463b6d68c6
codemsell <mbarlotta@gmail.com>
2026-02-09 17:07:32 -0500

    add max cycles

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn main() {
     info!("bootstrap: {}", args.bootstrap);
     info!("messageLocation: {}", args.message_location);
     info!("runOnce: {}", args.run_once);
+    info!("maxCycles: {}", args.max_cycles);
     info!("delayInMillis: {}", args.delay_millis);
     info!("noDeleteFiles: {}", args.no_delete_files);
 
@@ -27,6 +28,7 @@ fn main() {
         .keep_running(!args.run_once)
         .delete_files(!args.no_delete_files)
         .poll_interval_millis(args.delay_millis)
+        .max_poll_cycles(args.max_cycles)
         .build();
 
     // poll directory
